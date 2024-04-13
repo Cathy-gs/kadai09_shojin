fb900bd9c4776ccd31dd83f8d98cfce1ceede542
Cathy-gs <159711960+Cathy-gs@users.noreply.github.com>
2024-04-13 16:49:47 +0900

    Delete loop directory

--- loop/common/footer.php
+++
@@ -1,5 +0,0 @@
-<footer class="footer">
-  <div class="container">
-    <p class="text-muted">© 2024 LOOP</p>
-  </div>
-</footer>
--- loop/main/coontents.php
+++
@@ -1,7 +0,0 @@
-<div class="card " style="width: 32rem;">
-  <img src="img/main_1.png" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-</div>
